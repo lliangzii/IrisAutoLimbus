eb4becf380ac56a3fe4e080540c6cfed3f89cc57
liangzi <breezecanyve@163e.com>
2025-03-10 17:13:47 +0800

    -

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from logger import get_logger
 from setconfig import load_config
 
-if __name__ == '__main__':
+#初始化loger与config
+def init_config():
     logger = get_logger("DEBUG")
     logger.info('启动中...')
 
@@ -10,13 +11,14 @@ if __name__ == '__main__':
 
     logger.info('=============================================')
     logger.info('欢迎使用IrisAutoLimbus!')
-
     auther = config["information"]["auther"]
     logger.info(f'作者：{auther}')
-
     version = config["information"]["version"]
     logger.info(f'当前版本：{version}')
     logger.info('=============================================')
     log_level = config["logging"]["level"]
     logger.setLevel(log_level)
     logger.info(f'当前日志级别：{log_level}')
+
+if __name__ == '__main__':
+    init_config()
